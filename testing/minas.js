5657ada65f1ba8a91e9ab99f0d3a1a2cdd493139
AngelDaviid <angel_dvalenciae@soy.sena.edu.co>
2025-08-25 21:06:27 -0500

    cambios testing

--- testing/minas.js
+++ testing/minas.js
@@ -2,21 +2,27 @@ const tablero = document.getElementById("tablero");
 const mensaje = document.getElementById("mensaje");
 const btnNuevo = document.getElementById("nuevoJuego");
 
+//arreglo (celdas) el cual me almacenara cada div
 let celdas = [];
+//arreglo (minas) el cual me almacena las minas
 let minas = [];
+//variable la cual por cada celda en verde me guardara y sumara el puntaje
 let puntos = 0;
+//tamaño de cada celda
 const size = 5;
+//numero de minas
 const numMinas = 5;
-let juegoActivo = true;
+//variable booleana para blonquear el tablero al perder
+let juegoActivo = true
 
+// esta funcion me genera el tablero, itera y genera cada celda, agrega las minas y me almacena el puntaje
 function generarTablero() {
     tablero.innerHTML = "";
     celdas = [];
     minas = generarMinas();
     puntos = 0;
     juegoActivo = true;
-    mensaje.textContent = "¡Cuidado con las minas invertidas!";
-    mensaje.textContent = "En la consola puedes ver las posiciones de las minas"
+    mensaje.textContent = "¡Encuentra las casillas seguras!";
 
     for (let i = 0; i < size * size; i++) {
         const celda = document.createElement("div");
@@ -28,6 +34,8 @@ function generarTablero() {
     }
 }
 
+
+//esta funcion me crea las minas, se utiliza un while para generar un numero de minas contadas.
 function generarMinas() {
     let posiciones = [];
     while (posiciones.length < numMinas) {
@@ -36,29 +44,32 @@ function generarMinas() {
             posiciones.push(r);
         }
     }
-    console.log(posiciones);
     return posiciones;
 }
 
-function clickCelda(e) {
-    if (!juegoActivo) return;
-    const index = Number(e.target.dataset.index);
 
-    if (minas.includes(index)) {
-        e.target.classList.add("revelada");
-        e.target.textContent = "✅";
-        mensaje.textContent = `¡Perdiste! Puntos: ${puntos}`;
-        juegoActivo = false;
+// logica del juego, se detecta si le di click a una celda limpia o encima de una mina, si es mina = perder, sino continua el juego y suma puntos
+function clickCelda(e) {
+    if(!juegoActivo) return;
+    const index = e.target.dataset.index;
+    if (minas.includes(Number(index))) {
+        e.target.classList.add("mina");
+        mensaje.textContent = 💥 ¡Perdiste! Puntos: ${puntos};
         revelarMinas();
+        juegoActivo = false;
+
+        setTimeout(() => {
+            generarTablero();
+        }, 1500);
+
     } else {
         if (!e.target.classList.contains("revelada")) {
-            e.target.classList.add("mina");
-            e.target.textContent = "💣";
+            e.target.classList.add("revelada");
             puntos++;
             mensaje.textContent = `Puntos: ${puntos}`;
 
             if(puntos === size * size - minas.length){
-                mensaje.textContent = `!Ganaste¡ Puntos: ${puntos}`
+                mensaje.textContent = `¡Ganaste! Puntos: ${puntos}`
                 juegoActivo = false;
 
                 setTimeout(() => {
@@ -69,13 +80,15 @@ function clickCelda(e) {
     }
 }
 
+
+//recorre las posiciones donde habian minas y les cambia el estilo
 function revelarMinas() {
     minas.forEach(i => {
-        celdas[i].classList.add("revelada");
-        celdas[i].textContent = "";
+        celdas[i].classList.add("mina");
+        celdas[i].textContent = "💣";
     });
 }
 
-btnNuevo.addEventListener("click", revelarMinas);
+btnNuevo.addEventListener("click", generarTablero);
 
 generarTablero();
